66645569e560a89b446e4d729046f43e1487028b
Aryan Maftooh <138421552+aryanmaftooh@users.noreply.github.com>
2026-05-16 03:39:43 +0330

    Fix import URL for exit_node.ts

--- run_me.ts
+++ run_me.ts
@@ -1,4 +1,4 @@
-import handler from "https://aryanmaftoh.ir/exit_node.ts"; // import the default export
+import handler from "https://aryanmaftooh.ir/exit_node.ts"; // import the default export
 import { serve } from "https://deno.land/std@0.208.0/http/server.ts";
 
 serve(handler, { port: 8080 });
